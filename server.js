5fc5a29b1b9c22607ca47938b13b78ebf6d1083f
Drew Rothstein <drew.rothstein@gmail.com>
2013-08-24 20:28:15 -0400

    added and enabled cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
+    cors = require('cors'),
     wines = require('./routes/employee');
  
 var app = express();
 
-app.get('/employees/:id/reports', wines.findByManager);
-app.get('/employees/:id', wines.findById);
-app.get('/employees', wines.findAll);
+app.get('/employees/:id/reports', cors(), wines.findByManager);
+app.get('/employees/:id', cors(), wines.findById);
+app.get('/employees', cors(), wines.findAll);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
